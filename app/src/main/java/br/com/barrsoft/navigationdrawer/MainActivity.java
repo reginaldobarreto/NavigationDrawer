42c5256f4f2a35197965e002b0031556866a8dbd
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-10-18 17:15:25 -0300

    adicionado funcionalidade aos menus

--- app/src/main/java/br/com/barrsoft/navigationdrawer/MainActivity.java
+++ app/src/main/java/br/com/barrsoft/navigationdrawer/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.barrsoft.navigationdrawer;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     @BindView(R.id.drawerLayout)
     DrawerLayout drawerLayout;
+    @BindView(R.id.navigationView)
+    NavigationView navigationView;
 
 
     @Override
@@ -27,16 +34,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout, R.string.open_drawer, R.string.close_drawer);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+
+                    case R.id.menu_inbox:
+                        Toast.makeText(getApplicationContext(), R.string.inbox, Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.menu_sent:
+                        Toast.makeText(getApplicationContext(), R.string.sent, Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.menu_spam:
+                        Toast.makeText(getApplicationContext(), R.string.spam, Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.menu_trash:
+                        Toast.makeText(getApplicationContext(), R.string.trash, Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.menu_favorite:
+                        Toast.makeText(getApplicationContext(), R.string.favorites, Toast.LENGTH_LONG).show();
+                        break;
+                    default:
+                        return false;
+
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
     }
+
 
     @Override
     public void onBackPressed() {
